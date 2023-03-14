13b7a4a0276a875a4a85a8b639101db3bf9e7abf
Lak9422 <127815510+Lak9422@users.noreply.github.com>
2023-03-14 11:21:26 +0530

    student-add.test.js

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -1,25 +1,30 @@
 import { Selector } from 'testcafe';
-process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
     .page`http://localhost:4401/student`
+    .beforeEach(async t => {
+        try {
+            await t.navigateTo('/dbinitialize');
+        } catch (error) {
+            console.log('Error: ', error);
+            await t.wait(3000);
+            await t.navigateTo('/dbinitialize');
+        }
+    });
 
 test('Testing add students', async t => {
+    await t.navigateTo('/addStudent');
+    await t.typeText('#student-id', '999999');
+    await t.typeText('#student-name', 'Pasindu Basnayaka');
+    await t.typeText('#student-age', '45');
+    await t.typeText('#student-Hometown', 'Catholic');
+    await t.click('#student-add');
 
-    await t.navigateTo("/dbinitialize");
+    await t.navigateTo('/student');
 
-    await t.navigateTo("/addStudent");
-    await t.typeText("#student-id", "999999");
-    await t.typeText("#student-name", "Pasindu Basnayaka");
-    await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "Catholic");
-    await t.click("#student-add");
-
-    await t.navigateTo("/student");
-
-    const table = Selector('#student-table')
+    const table = Selector('#student-table');
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).contains("Pasindu Basnayaka");
+    await t.expect(tdText).contains('Pasindu Basnayaka');
 });
